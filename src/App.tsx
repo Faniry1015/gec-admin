fbee88df307473a7400bec80a59d6d401ca9c50c
Faniry1015 <frandriaharimino@yahoo.com>
2025-11-28 10:44:22 +0300

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button variant="destructive" size='sm'>shadcn+</Button>
+        <button>normal button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
