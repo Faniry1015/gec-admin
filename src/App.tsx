801ab28cb5163cd452065d192192c5f54d517bbf
Faniry1015 <frandriaharimino@yahoo.com>
2025-11-28 11:21:29 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { Button } from './components/ui/button'
+
+import { useEffect, useState } from "react";
+import { collection, getDocs, addDoc  } from "firebase/firestore";
+import { db } from "./firebase-config";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const [users, setUsers] = useState<string[]>([]);
 
-  return (
-    <>
-      <div>
-        <Button variant="destructive" size='sm'>shadcn+</Button>
-        <button>normal button</button>
-      </div>
-    </>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      const snap = await getDocs(collection(db, "users"));
+      snap.forEach((doc) => {
+        const name = (doc.data().name ?? "") as string;
+        setUsers((prev) => [...prev, name]);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  const AddUser = async () => {
+    await addDoc(collection(db, "users"), {
+      name: "Innovation",
+      age: 22,
+    });
+  }
+
+  return <>
+    <h1>Firebase prêt !</h1>
+    <Button onClick={() =>AddUser()}>AddDoc</Button>
+    {users.length}
+    <ul>
+      {users.map((user, index) => (
+        <li key={index}>{user}</li>
+      ))}
+    </ul>
+  </>
 }
 
-export default App
+export default App;
--- src/services/firebase_service.ts
+++ src/services/firebase_service.ts
@@ -1,7 +1,1 @@
-import { db } from "@/firebase-config";
-import { collection, addDoc } from "firebase/firestore";
 
-await addDoc(collection(db, "users"), {
-  name: "Innovation",
-  age: 22,
-});
